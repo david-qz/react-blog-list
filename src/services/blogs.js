a9836c8b050bfc1f869dba2d3996e06e6821cb2a
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-09-19 12:30:19 -0700

    Fetch and display author with each post

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -3,7 +3,7 @@ import { client, checkError } from './client';
 export async function getBlogs() {
   const response = await client
     .from('blogs')
-    .select('*');
+    .select('*, author: authors(name)');
 
   return checkError(response);
 }
